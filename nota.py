2c458d874dcc1ba7a89b6227c090fbd1e16be237
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-06-13 20:38:23 -0300

    Create nota.py

---
+++ nota.py
@@ -0,0 +1,8 @@
+no1 = float(input("Digite a primeira nota do aluno: "))
+no2 = float(input("Digite a segunda nota do aluno: "))
+
+calc = (no1+no2)/2     #de acordo com a procedencia (parenteses antes)
+
+print(f"As notas foram {no1} e {no2}\nA média do aluno foi {calc:.1f}")
+
+#Um programa que leia duas notas de um aluno e calcule a média
