6ec7e98a1c97e6b0ceee9b0433cf205c63c4261c
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-06-13 20:37:21 -0300

    Create tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,10 @@
+n1 = int(input("Digite um número: "))
+
+print(f"A tabuada do número {n1} é:\n ")
+for num in range(11):
+    tab = num * n1
+    print(f"{num} X{n1:2} = {tab}") #depois do x dois caracteres
+
+
+
+#Ler um número e dar a tabuada dele
